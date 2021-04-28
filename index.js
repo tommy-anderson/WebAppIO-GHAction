1d8f4a067e069977a1d318230fcb843865232c30
tommy-anderson <alexursu@planable.io>
2021-04-28 10:43:44 +0300

    👌 First attempt at layerCI GH action

---
+++ index.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core');
+
+try {
+  const apiKey = core.getInput('apiKey')
+  const apiExtra = core.getInput('apiExtra')
+  const buttons = core.getInput('buttons')
+  const branch = core.getInput('branch')
+  const ref = core.getInput('ref')
+  
+  fetch(
+    'https://layerci.com/api/v1/run/repo_name?layertoken='+apiKey,
+    {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'branch': branch,
+            'ref': ref,
+            'accept_buttons': buttons,
+            'extra': apiExtra,
+        }),
+    }
+).then(res => res.json()).then(json => console.log(json))
+
+
+} catch (error) {
+  core.setFailed(`Action failed with error ${error}`);
+}
+
+
+
+
+
+
